85e07de4aeb4d1ff3561e9a7405fdb28a20dfe17
shahawlhs <shaha@wlhs.wlwv.k12.or.us>
2023-03-01 10:39:17 -0800

    Lesson 6.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] names = {"Alice", "Bob", "Cam", "Don"};
+
+        for(int i = 0; i < names.length; i++){
+            names[i] = names[i] + " Smith";
+            System.out.println(names[i]);
+        }
+
+        for(String name: names){
+            name = names + " Smith";
+            System.out.println(name);
+        }
     }
 }
